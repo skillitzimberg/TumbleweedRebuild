305d5199b8dcb7298bf396390ae89f45614f30c2
Scott Bergler <commandinghands@gmail.com>
2019-03-01 14:54:58 -0800

    add links for all Routes in Nav component.

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -5,6 +5,10 @@ function Nav(){
   return (
     <div className="navigation">
       <Link to="/admin">Admin</Link>
+      <Link to="/admin/customers">Customers</Link>
+      <Link to="/admin/orders">Orders</Link>
+      <Link to="/admin/products">Products</Link>
+      <Link to="/admin/locations">Locations</Link>
     </div>
   );
 }
