e7f3f1ccb2c981808b8c8f487328421d5165026f
Scott Bergler <commandinghands@gmail.com>
2019-03-01 13:30:49 -0800

    add starting content to Admin component.

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+// import PropTypes from "prop-types";
 
+function Admin() {
+  return (
+    <div>
+      <div className="container">
+        <p>Admin Works</p>
+      </div>
+    </div>
+  );
+}
+
+export default Admin;
