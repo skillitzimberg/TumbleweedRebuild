9ab9a68493844d546ba9f3e93632f5506678a227
Scott Bergler <commandinghands@gmail.com>
2019-03-01 14:52:33 -0800

    add Routes to Customers, Products, Orders, and Locations to App component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Admin from "./../Admin/Admin";
 import Error404 from "./../Error404";
 import Header from "./../Header/Header";
+import Customers from "./../Customers/Customers";
+import Products from "./../Products/Products";
+import Orders from "./../Orders/Orders";
+import Locations from "./../Locations/Locations";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
       <p>App Works</p>
       <Switch>
         <Route exact path="/admin" component={Admin} />
+        <Route exact path="/admin/customers" component={Customers} />
+        <Route exact path="/admin/products" component={Products} />
+        <Route exact path="/admin/orders" component={Orders} />
+        <Route exact path="/admin/locations" component={Locations} />
         <Route component={Error404} />
       </Switch>
     </div>
