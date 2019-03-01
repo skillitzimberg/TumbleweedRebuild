9e778ab80706a144b2728d6b161fe4e0e3bcaf7a
Scott Bergler <commandinghands@gmail.com>
2019-03-01 13:46:01 -0800

    fix console errors related to incorrect import declaration.

---
+++ src/components/Customers/Customer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Customer() {
+  return (
+    <div>
+      <div className="container">
+        <p>Customer Works</p>
+      </div>
+    </div>
+  );
+}
+
+export default Customer;
