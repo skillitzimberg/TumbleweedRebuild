31aeee3b0112f6c3ee8cc7f18234553a5afeaef7
Scott Bergler <commandinghands@gmail.com>
2019-03-01 13:56:14 -0800

    add starting content to Product component.

---
+++ src/components/Products/Product.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Product() {
+  return (
+    <div>
+      <div className="container">
+        <p>Product Works</p>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
