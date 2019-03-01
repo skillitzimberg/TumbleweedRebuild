734e0ff8818357b15af6efcf5d9151c6929182f2
Scott Bergler <commandinghands@gmail.com>
2019-03-01 13:46:31 -0800

    add starting content to Locations component.

---
+++ src/components/Locations/Locations.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Location from "./../Locations/Location";
+// import PropTypes from "prop-types";
+
+function Locations() {
+  return (
+    <div>
+      <div className="container">
+        <p>Locations Works</p>
+        <Location />
+      </div>
+    </div>
+  );
+}
+
+export default Locations;
