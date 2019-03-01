0b2d4d3f06425242835914a3a5095b8330a7907a
Scott Bergler <commandinghands@gmail.com>
2019-03-01 15:43:20 -0800

    add a masterCustomerList file as fake Customers database.

---
+++ src/components/Admin/masterCustomerList.js
@@ -0,0 +1,23 @@
+export const masterCustomerList = [
+  {
+    firstName: "Scott",
+    lastName: "Bergler",
+    phone: "503.890.5118",
+    email: "commandinghands@gmail.com",
+    postalCode: "97211"
+  },
+  {
+    firstName: "Millicent",
+    lastName: "Zimdars",
+    phone: "971.276.9712",
+    email: "anemail@emailorg",
+    postalCode: "97123"
+  },
+  {
+    firstName: "Wayne",
+    lastName: "Coyne",
+    phone: "435.912.4083",
+    email: "bitcoyne@me.com",
+    postalCode: "56812"
+  },
+];
