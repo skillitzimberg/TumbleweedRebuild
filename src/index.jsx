7fed52576f8bf1e07903f1bf2c6e6064dcaa806e
Scott Bergler <commandinghands@gmail.com>
2019-03-01 13:04:57 -0800

    add basic content to start src/index.jsx.

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import { AppContainer } from "react-hot-loader";
-import { HashRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <HashRouter>
+    <HashRouter>
+      <Provider store={store}>
         <Component/>
-      </HashRouter>
-    </AppContainer>,
+      </Provider>
+    </HashRouter>,
     document.getElementById("react-app-root")
   );
 };
