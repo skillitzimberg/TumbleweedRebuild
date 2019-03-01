fdeff2a1c0ec459c207bd6974a24413becde5cd5
Scott Bergler <commandinghands@gmail.com>
2019-03-01 13:54:14 -0800

    add starting content to Order component.

---
+++ src/components/Orders/Order.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Order() {
+  return (
+    <div>
+      <div className="container">
+        <p>Order Works</p>
+      </div>
+    </div>
+  );
+}
+
+export default Order;
