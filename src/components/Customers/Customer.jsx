fac35f206d841b139c6317712f3bd0660196d08b
Scott Bergler <commandinghands@gmail.com>
2019-03-01 16:04:52 -0800

    add markup to display props.

--- src/components/Customers/Customer.jsx
+++ src/components/Customers/Customer.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 // import PropTypes from "prop-types";
 
-function Customer() {
+function Customer(props) {
   return (
     <div>
       <div className="container">
         <p>Customer Works</p>
+        <h3>props.firstName props.lastName</h3>
+        <p>props.phone</p>
+        <p>props.email</p>
+        <p>props.postalCode</p>
       </div>
     </div>
   );
 }
+
+Customer.propTypes = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  postalCode: PropTypes.string
+};
 
 export default Customer;
