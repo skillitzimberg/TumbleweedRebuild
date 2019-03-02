68a316ddeeaaf9130ff024a68c0a44c18c95b797
Scott Bergler <commandinghands@gmail.com>
2019-03-01 16:04:00 -0800

    pass masterCustomerList as props to CustomerList.

---
+++ src/components/Customers/CustomerList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Customer from "./../Customers/Customer";
+
+function CustomerList(props) {
+  return (
+    <div>
+      <div className="container">
+        <p>CustomerList Works</p>
+        {props.customers.map((customer) =>
+          <Customer
+            firstName={customer.firstName}
+            lastName={customer.lastName}
+            phone={customer.phone}
+            email={customer.email}
+            postalCode={customer.postalCode}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+CustomerList.propTypes = {
+  customers: PropTypes.array
+};
+
+export default CustomerList;
