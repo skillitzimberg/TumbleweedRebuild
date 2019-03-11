7c39b289956df51bcc598a5cdabf2e69129f17f4
Scott Bergler <commandinghands@gmail.com>
2019-03-11 07:44:49 -0700

    add id key/value.

--- src/components/App/masterCustomerList.js
+++ src/components/App/masterCustomerList.js
@@ -4,20 +4,23 @@ export const masterCustomerList = [
     lastName: "Bergler",
     phone: "503.890.5118",
     email: "commandinghands@gmail.com",
-    postalCode: "97211"
+    postalCode: "97211",
+    id: 1
   },
   {
     firstName: "Millicent",
     lastName: "Zimdars",
     phone: "971.276.9712",
     email: "anemail@emailorg",
-    postalCode: "97123"
+    postalCode: "97123",
+    id: 2
   },
   {
     firstName: "Wayne",
     lastName: "Coyne",
     phone: "435.912.4083",
     email: "bitcoyne@me.com",
-    postalCode: "56812"
+    postalCode: "56812",
+    id: 3
   },
 ];
