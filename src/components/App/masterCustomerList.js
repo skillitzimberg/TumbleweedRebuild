a3dc3d193dd9f917c3b13e9257aca56eef5cf3b1
Scott Bergler <commandinghands@gmail.com>
2019-03-12 09:16:37 -0700

    refactor masterCustomerList to be JSON instead of array.

--- src/components/App/masterCustomerList.js
+++ src/components/App/masterCustomerList.js
@@ -1,27 +1,24 @@
 import { v4 } from "uuid";
-export const masterCustomerList = [
-  {
+export const masterCustomerList = {
+  v4(): {
     firstName: "Scott",
     lastName: "Bergler",
     phone: "503.890.5118",
     email: "commandinghands@gmail.com",
-    postalCode: "97211",
-    id: v4()
+    postalCode: "97211"
   },
-  {
+  v4(): {
     firstName: "Millicent",
     lastName: "Zimdars",
     phone: "971.276.9712",
     email: "anemail@emailorg",
-    postalCode: "97123",
-    id: v4()
+    postalCode: "97123"
   },
-  {
+  v4(): {
     firstName: "Wayne",
     lastName: "Coyne",
     phone: "435.912.4083",
     email: "bitcoyne@me.com",
-    postalCode: "56812",
-    id: v4()
+    postalCode: "56812"
   },
-];
+};
