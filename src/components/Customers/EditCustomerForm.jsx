c119e0c8e589cc6cb01afaba881c38dceb22ae05
Scott Bergler <commandinghands@gmail.com>
2019-03-13 11:59:49 -0700

    refactor edit form.

--- src/components/Customers/EditCustomerForm.jsx
+++ src/components/Customers/EditCustomerForm.jsx
@@ -3,87 +3,67 @@ import PropTypes from "prop-types";
 
 class EditCustomerForm extends React.Component {
   constructor(props) {
+    console.log(props);
     super(props);
-
-
     this.state = {
-      customerToEdit: props.customer
+      firstName: this.props.customer.firstName,
+      lastName: this.props.customer.lastName,
+      phone: this.props.customer.phone,
+      email: this.props.customer.email,
+      postalCode: this.props.customer.postalCode,
+      id: this.props.customer.id
     };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   handleEditFormSubmission(event) {
     event.preventDefault();
 
-    let _firstName = null;
-    let _lastName = null;
-    let _phone = null;
-    let _email = null;
-    let _postalCode = null;
-
     this.props.onEditCustomer(
       {
-        firstName: this._firstName.value,
-        lastName: this._lastName.value,
-        phone: this._phone.value,
-        email: this._email.value,
-        postalCode: this._postalCode.value,
-        id: this.state.customerToEdit.id
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        phone: this.state.phone,
+        email: this.state.email,
+        postalCode: this.state.postalCode,
+        id: this.state.id
       }
     );
-
-    this._firstName.value = "";
-    this._lastName.value = "";
-    this._phone.value = "";
-    this._email.value = "";
-    this._postalCode.value = "";
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleEditFormSubmission}>
-          <input
-            type="text"
-            id="firstName"
-            placeholder={this.state.customerToEdit.firstName}
-            ref={(input) => {this._firstName = input;}}
-            required/>
 
-          <input
-            type="text"
-            id="lastName"
-            placeholder={this.state.customerToEdit.lastName}
-            ref={(input) => {this._lastName = input;}}
-            required/>
-
-          <input
-            type="text"
-            id="phone"
-            placeholder={this.state.customerToEdit.phone}
-            ref={(input) => {this._phone = input;}}
-            pattern="[0-9]{10,15}"
-            required/>
-
-          <input
-            type="text"
-            id="email"
-            placeholder={this.state.customerToEdit.email}
-            ref={(input) => {this._email = input;}}
-            pattern="[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}"
-            required/>
-
-          <input
-            type="text"
-            id="postalCode"
-            placeholder={this.state.customerToEdit.postalCode}
-            pattern="[A-Za-z0-9-]{5,10}"
-            ref={(input) => {this._postalCode = input;}}/>
-
-          <button type="submit">Edit Customer</button>
-        </form>
-      </div>
+      <form onSubmit={this.handleEditFormSubmission}>
+        <label>
+          Name:
+          <input name="firstName" type="text" placeholder="Type it" value={this.state.firstName} onChange={this.handleChange} />
+          <input name="lastName" type="text" placeholder="Type it" value={this.state.lastName} onChange={this.handleChange} />
+        </label>
+        <input name="phone" type="text" placeholder="Type it" value={this.state.phone} onChange={this.handleChange} />
+        <input name="email" type="text" placeholder="Type it" value={this.state.email} onChange={this.handleChange} />
+        <input name="postalCode" type="text" placeholder="Type it" value={this.state.postalCode} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
     );
   }
 }
