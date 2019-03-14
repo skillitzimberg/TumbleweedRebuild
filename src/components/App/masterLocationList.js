e69642bcc7113ac1a5b58c7468a3753981afc0c5
Scott Bergler <commandinghands@gmail.com>
2019-03-14 08:24:29 -0700

    make slightly better demo data.

--- src/components/App/masterLocationList.js
+++ src/components/App/masterLocationList.js
@@ -1,23 +1,23 @@
 export const masterLocationList = {
   "9ef94h": {
-    locationName: "farmer market one",
+    locationName: "San Jose Farmer's Market",
     locationAddress: "2323 something st",
     locationPostalCode: "234fq3fq",
-    locationDescription: "lorem stuff and things.",
+    locationDescription: "Roasted hatch peppers, jewelry, sundry.",
     id: "9ef94h"
   },
   "9wnv90nweo": {
-    locationName: "Another market",
+    locationName: "Montevilla Swap Meet",
     locationAddress: "23 Juniper St",
     locationPostalCode: "092834",
     locationDescription: "lorem stuff and things.",
     id: "9wnv90nweo"
   },
   "3nf9fn38nf3": {
-    locationName: "bake sale",
+    locationName: "Alamosa Farmer's Market",
     locationAddress: "87 Holladay Ave",
     locationPostalCode: "3245234",
-    locationDescription: "lorem stuff and things.",
+    locationDescription: "Big city, big market.",
     id: "3nf9fn38nf3"
   },
 };
--- src/components/App/masterProductList.js
+++ src/components/App/masterProductList.js
@@ -1,26 +1,26 @@
 export const masterProductList = {
   "hasd98hsad9n": {
-    productName: "A thing",
+    productName: "Cinnamon roll",
     productType: "desert",
     ingredients: ["sugar", "flour", "butter", "olives"],
     productDescription: "lorem stuff and things.",
-    productPrice: 5,
+    productPrice: 5.34,
     id: "hasd98hsad9n"
   },
   "99234092j9e": {
-    productName: "Another thing",
-    productType: "sourdough",
+    productName: "Challa",
+    productType: "traditional",
     ingredients: ["sugar", "flour", "butter", "sour"],
     productDescription: "mother stuff and things.",
-    productPrice: 3,
+    productPrice: 3.99,
     id: "99234092j9e"
   },
   "909348e90fj94jfq93u": {
     productName: "raisin scone",
-    productType: "breakfast",
+    productType: "pastry",
     ingredients: ["sugar", "flour", "butter", "raisins"],
     productDescription: "raisins . . . for breakfast.",
-    productPrice: 2,
+    productPrice: 2.50,
     id: "909348e90fj94jfq93u"
   },
 };
