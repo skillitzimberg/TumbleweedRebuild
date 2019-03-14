9366c8ea40e01779a340049251cedddb5e543331
Scott Bergler <commandinghands@gmail.com>
2019-03-14 04:36:03 -0700

    add need files for Orders.

---
+++ src/components/Orders/OrderList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+
+function ProductList({products, onDeletingProduct}) {
+
+  function deleteProduct(productId) {
+    event.preventDefault();
+
+    onDeletingProduct(productId);
+  }
+
+  return (
+    <div className="container">
+      {Object.keys(products).map((productId) => {
+        let product = products[productId];
+        return <div key={v4()}>
+          <Link
+            to={`/admin/products/${product.id}`}
+          >
+            <p>{product.productName}</p>
+          </Link>
+          <button onClick={() => {deleteProduct(product.id);}}>Delete</button>
+        </div>;
+      }
+      )}
+    </div>
+  );
+}
+
+ProductList.propTypes = {
+  products: PropTypes.object,
+  onDeletingProduct: PropTypes.func
+};
+
+export default ProductList;
